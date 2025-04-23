5c790561408349e09652685de452aa0e5d103429
Fernando-Oli <primoflro@gmail.com>
2025-04-23 09:42:23 -0300

    removendo arquivos grandes do histórico

--- src/app/utils/menuStructure.ts
+++
@@ -1,9 +0,0 @@
-const menuStructure = [
-    { title: "LOCALIZAÇÃO", submenu: ['MAPA 2D', 'MAPA SATÉLITE', 'ULTRATOUR'], caminho: "/localizacao" },
-    { title: "IMAGENS", submenu: ['ÁREAS COMUNS', 'APARTAMENTOS', 'TOUR VIRTUAL'], caminho: "/imagens" },
-    { title: "PROJETO", submenu: ['IMPLANTAÇÃO', 'PLANTAS', 'COMPARE'], caminho: "/projeto" },
-    { title: "VÍDEOS", submenu: ['CONCEITO'], caminho: "/videos" },
-    { title: "INFORMAÇÕES", submenu: ['FICHA TÉCNICA', 'DIFERENCIAIS', 'INSTITUCIONAL'], caminho: "/infos" },
-  ];
-
-export default menuStructure;
